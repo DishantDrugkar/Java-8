f61e428059359b404c2ff7ab19e1ecdf15a054c6
Dishant Drugkar <114806306+DishantDrugkar@users.noreply.github.com>
2025-12-16 13:34:48 +0530

    Add MyThread class to demonstrate sleep method
    Implement MyThread class with sleep method in multithreading.

---
+++ multithreading/SleepMethod.java
@@ -0,0 +1,21 @@
+package org.example.multithreading;
+
+public class MyThread extends Thread{
+    @Override
+    public void run() {
+        System.out.println("Thread is Running");
+        for(int i=0; i<=5; i++){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start();
+    }
+}
